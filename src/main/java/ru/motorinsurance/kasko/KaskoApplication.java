05ccb3ac1a323f6d7fd228e3a3a8f90717fe079e
Comrade-Ivan <1335308@gmail.com>
2025-06-10 13:04:16 +0300

    init commit

---
+++ src/main/java/ru/motorinsurance/kasko/KaskoApplication.java
@@ -0,0 +1,13 @@
+package ru.motorinsurance.kasko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaskoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/motorinsurance/kasko/KaskoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.motorinsurance.kasko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
